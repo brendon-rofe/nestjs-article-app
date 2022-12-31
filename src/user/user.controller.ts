be35bfe1997362ee484dd5564f049e89d84cacd3
Bravo1B9 <b1@xgmi.com>
2022-12-31 15:05:25 +0000

    update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,7 @@
 import { Body, Controller, Post } from "@nestjs/common";
-import { CreateUserDto } from "./dto/createUser.dto";
 import { UserService } from "./user.service";
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @Post()
-  createUser(@Body() dto: CreateUserDto) {
-    return this.userService.createUser(dto);
-  }
 }
