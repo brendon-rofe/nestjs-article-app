7e7d2b847a468fec29640a314fb69c6bebe72306
Bravo1B9 <b1@xgmi.com>
2023-01-04 07:50:38 +0000

    Initial password setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { UserService } from "src/user/user.service";
 import { RegisterDto } from "./dto/register.dto";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   public async register(dto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
     const newUser = await this.userService.createUser({
-      ...dto
-    })
+      ...dto,
+      password: hashedPassword
+    });
     return newUser;
+  }
+
+  private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword
+    );
+    if (!isPasswordMatching) {
+      throw new HttpException('Wrong credentials provided',
+      HttpStatus.BAD_REQUEST
+      );
+    }
   }
 }
